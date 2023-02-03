dd11ccd2a80394e17863f4dba3598629c765cd8c
goldbocman <hxfcndjd863@gmail.com>
2023-02-03 18:45:08 +0200

    start

---
+++ main.py
@@ -0,0 +1,44 @@
+import sys
+import requests
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)  # Загружаем дизайн
+
+        self.refresh_map()
+
+    def keyPressEvent(self, event) -> None:
+        pass
+
+    def refresh_map(self, event):
+        map_params = {
+            "ll": ','.join(map(str, self.map_ll)),
+            "l": self.map_l,
+            'z': self.map_zoom
+        }
+        session = requests.Session()
+        retry = Retry(total=10, connect=5, backoff_factor=0.5)
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        response = session.get('https://static-maps.yandex.ru/1.x/',
+                               params=map_params)
+        with open('tmp.png', mode='wb') as tmp:
+            tmp.write(response.content)
+
+        pixmap = QPixmap()
+        pixmap.load('tmp.png')
+        self.g_map.setPixmap(pixmap)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
